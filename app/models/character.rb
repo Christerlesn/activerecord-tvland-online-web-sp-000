abdf55bbaa3e8e950b33ef8ea5b7b1fa6b1dfdfd
Christerle Nounoute <christerlesn@gmail.com>
2021-01-04 10:51:28 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,6 +4,7 @@ class Character < ActiveRecord::Base
   belongs_to :show
 
   def say_that_thing_you_say
-    binding.pry
+    the_say = "#{self.name} always says: #{self.catchphrase}"
+    the_say
   end
 end
